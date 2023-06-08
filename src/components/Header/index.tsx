cf973469357c3d5188e414f81627f656af515bb4
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-06-08 19:12:48 -0300

    Componente: SignIn Button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,27 @@
-import Link from "next/link";
-import Image from "next/image";
-import { FiGithub } from "react-icons/fi";
+import Link from 'next/link'
+import Image from 'next/image'
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss'
+import { SignInButton } from '../SignInButton'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Link href="/">
-          <Image src="/images/logo.svg" alt="Logo" className={styles.logo} width={120} height={40} />
+          <Image src="/images/logo.svg" alt="Logo" width={120} height={40} />
         </Link>
         <nav>
           <Link className={styles.active} href="/">
-           Home
+            Home
           </Link>
           <Link className={styles.link} href="/posts">
             Posts
           </Link>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
-  );
+  )
 }
